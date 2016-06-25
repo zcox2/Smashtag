b032441fa3ba25cc199b043a5b311c48bca508e1
Zachary Cox <zcox2@illinois.edu>
2016-06-24 20:36:03 -0500

    Added swipe to delete on search history and icons to tab bar

--- Smashtag/SearchesTableViewController.swift
+++ Smashtag/SearchesTableViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class SearchesTableViewController: UITableViewController {
     var searchTableCounter: Int = 0
- 
+    
     var searches = [String]() {
         didSet {
-            self.tableView.reloadData()
+            if searches.isEmpty {
+                self.tableView.reloadData()
+            }
         }
     }
     let defaults = NSUserDefaults.standardUserDefaults()
@@ -22,10 +24,10 @@ class SearchesTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
@@ -37,12 +39,16 @@ class SearchesTableViewController: UITableViewController {
         super.viewWillAppear(animated)
         retrieveSearchHistory()
     }
-
+    
     
     @IBAction func clearSearches(sender: UIBarButtonItem) {
         searches.removeAll()
         defaults.setObject(searches, forKey: "Searches")
-
+    }
+    
+    func removeSearchAtIndex(index: Int) {
+        searches.removeAtIndex(index)
+        defaults.setObject(searches, forKey: "Searches")
     }
     
     func retrieveSearchHistory() {
@@ -52,19 +58,19 @@ class SearchesTableViewController: UITableViewController {
             }
         }
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return searches.count
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("SearchCell", forIndexPath: indexPath)
@@ -73,7 +79,7 @@ class SearchesTableViewController: UITableViewController {
             return searchCell
         }
         // Configure the cell...
-
+        
         return cell
     }
     
@@ -89,45 +95,47 @@ class SearchesTableViewController: UITableViewController {
         print("deinitialized, we're at \(searchTableCounter) tables")
     }
     
-
-    /*
+    
+    
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
+    
+    
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            removeSearchAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+        //        else if editingStyle == .Insert {
+        //            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        //        }
     }
-    */
-
+    
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -145,6 +153,6 @@ class SearchesTableViewController: UITableViewController {
             tweetTableVC.searchText = sender as? String
         }
     }
-
-
+    
+    
 }
